e51ba7df186d5a7c032b9162936405a7b101e818
Szabados Péter <peet@peti-linux.(none)>
2011-02-16 18:16:29 +0100

    new file:   src/Options.cpp 	new file:   src/Options.h

---
+++ src/Options.cpp
@@ -0,0 +1,32 @@
+#include "Options.h"
+
+#include <iostream>
+#include <boost/program_options.hpp>
+
+
+Options::Options(int argc, char **argv) {
+	using namespace boost::program_options;
+	options_description desc;
+	desc.add_options()
+		("help", "produce help message")
+		("enable-dump,d", "enable dump file generation of the process")
+		("filename", value<std::string>(), "input file name")
+		;
+
+	positional_options_description p;
+	p.add("filename", 1);
+
+
+	variables_map vm;
+	store(command_line_parser(argc, argv).
+			options(desc).positional(p).run(), vm);
+	notify(vm);
+
+	if (vm.count("help") > 0) {
+		std::cerr << desc << std::endl;
+		exit(0);
+	}
+	enableDump_ = vm.count("enable-dump") > 0;
+	filename_ = vm["filaname"].as<std::string>();
+}
+
---
+++ src/Options.h
@@ -0,0 +1,16 @@
+#ifndef OPTIONS_H_
+#define OPTIONS_H_
+
+#include <string>
+
+class Options {
+	bool enableDump_;
+	std::string filename_;
+public:
+	Options(int argc, char **argv);
+
+	bool enableDump() { return enableDump; }
+	const std::string &filename() { return filename_; }
+};
+
+#endif /* OPTIONS_H_ */
