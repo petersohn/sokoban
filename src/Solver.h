dcd07319202fd31c0e3452b5dbae2cfabf5a8083
Peter Szabados <kangirigungi@gmail.com>
2011-03-04 21:58:20 +0100

    Solver rewrited.
    Calculator in Solver??

--- src/Solver.h
+++ src/Solver.h
@@ -1,18 +1,22 @@
 #ifndef SOLVER_H_
 #define SOLVER_H_
 
-#include "Status.h"
-#include "HeurCalculator.h"
-#include "Node.h"
+#include "QueueInterfaces.h"
+#include "Expander.h"
 #include <deque>
+
+class Status;
+class Node;
 
 class Solver {
 public:
-	std::deque<Node::Ptr> solve(const Status &status,
-			HeurCalculator::Ptr calculator,
-			bool enableLog = false, bool enableDump = false,
-			bool enableXDump = false);
+	typedef boost::shared_ptr<Solver> Ptr;
 
+	std::deque<Node::Ptr> solve(Status status);
+private:
+	// factory methods
+	virtual NodeQueue::Ptr createQueue() = 0;
+	virtual Expander::Ptr createExpander() = 0;
 };
 
 #endif /* SOLVER_H_ */
