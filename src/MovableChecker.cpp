61c6e2d6b4a0fba7a8d0c90d4ca54a1d00c67132
Peter Szabados <kangirigungi@gmail.com>
2014-10-13 21:23:51 +0200

    Less shared_ptr copying in MovableChecker

--- src/MovableChecker.cpp
+++ src/MovableChecker.cpp
@@ -2,19 +2,19 @@
 #include <unordered_set>
 
 class InternalChecker {
-	HeurCalculator::Ptr calculator_;
-	std::unordered_set<Point> volt_;
+	HeurCalculator& calculator_;
+	std::unordered_set<Point> calculated_;
 	bool isMovable(const Status &status, Point  p,
 		int &count);
 public:
-	InternalChecker(HeurCalculator::Ptr calculator):
+	InternalChecker(HeurCalculator& calculator):
 		calculator_(calculator)
 	{}
 	bool stoneMovable(const Status &status, Point p);
 };
 
 bool InternalChecker::stoneMovable(const Status &status, Point p) {
-	volt_.insert(p);
+	calculated_.insert(p);
 	int count = 0;
 	if (isMovable(status, p+Point::p10, count) &&
 			isMovable(status, p+Point::pm10, count) && count > 0)
@@ -28,11 +28,11 @@ bool InternalChecker::isMovable(const Status &status, Point  p,
 		int &count) {
 	if (status.value(p) == ftWall)
 		return false;
-	if (calculator_->calculateStone(status, p) >= 0)
+	if (calculator_.calculateStone(status, p) >= 0)
 			++count;
 	if (status.value(p) == ftFloor)
 		return true;
-	if (volt_.count(p) != 0)
+	if (calculated_.count(p) != 0)
 		return false;
 	return stoneMovable(status, p);
 }
@@ -40,7 +40,7 @@ bool InternalChecker::isMovable(const Status &status, Point  p,
 
 
 bool MovableChecker::check(const Status &status, Point p) {
-	InternalChecker ch(calculator_);
+	InternalChecker ch(*calculator_);
 	return ch.stoneMovable(status, p);
 }
 
