9552f39a28ca787ea3f50b060364d5d4dd97d036
Peter Szabados <kangirigungi@gmail.com>
2014-10-13 22:03:34 +0200

    Do not use shared_ptr in IndexedStatusList

--- src/IndexedStatusList.hpp
+++ src/IndexedStatusList.hpp
@@ -14,40 +14,24 @@
 template <class Value>
 class IndexedStatusList {
 private:
-	typedef std::unordered_multimap<Point, std::pair<Status::ConstPtr, Value>> IndexType;
+	typedef std::unordered_multimap<Point, std::pair<Status, Value>> IndexType;
 
 	IndexType index_;
 	size_t size_;
 
-	void copyFrom(const IndexedStatusList &other)
-	{
-		index_.clear();
-		for (const auto& elem: other.index_) {
-			Status::ConstPtr copied(new Status(*(elem.second.first)));
-			index_.insert(std::make_pair(elem.first, std::make_pair(copied, elem.second.second)));
-		}
-	}
 public:
 	typedef boost::iterator_range<typename IndexType::const_iterator> RangeType;
 
 	IndexedStatusList():size_(0) {}
-	IndexedStatusList(const IndexedStatusList &other)
-	{
-		copyFrom(other);
-	}
-	IndexedStatusList &operator=(const IndexedStatusList &other) {
-		copyFrom(other);
-		return *this;
-	}
-
+	IndexedStatusList(const IndexedStatusList&) = default;
+	IndexedStatusList& operator=(const IndexedStatusList&) = default;
 	IndexedStatusList(IndexedStatusList&&) = default;
 	IndexedStatusList& operator=(IndexedStatusList&&) = default;
 
-	void add(const Status &status, const Value& value)
+	void add(const Status& status, const Value& value)
 	{
-		Status::ConstPtr val(new Status(status));
 		for (Point  p: status.state()) {
-			index_.insert(std::make_pair(p, std::make_pair(val, value)));
+			index_.insert({p, {status, value}});
 		}
 		++size_;
 	}
@@ -56,24 +40,24 @@ public:
 		index_.clear();
 	}
 
-	bool hasSubStatus(const Status &status, Point p) const
+	bool hasSubStatus(const Status& status, Point p) const
 	{
 		RangeType range = index_.equal_range(p);
-		for (const auto& val: range) {
-			if (isSubStatus(*val.second.first, status)) {
+		for (const auto& value: range) {
+			if (isSubStatus(value.second.first, status)) {
 				return true;
 			}
 		}
 		return false;
 	}
 
-	std::vector<Value> getSubStatusValues(const Status &status, Point p) const
+	std::vector<Value> getSubStatusValues(const Status& status, Point p) const
 	{
 		std::vector<Value> result;
 		RangeType range = index_.equal_range(p);
-		for (const auto& val: range) {
-			if (isSubStatus(*val.second.first, status)) {
-				result.push_back(val.second.second);
+		for (const auto& value: range) {
+			if (isSubStatus(value.second.first, status)) {
+				result.push_back(value.second.second);
 			}
 		}
 		return result;
