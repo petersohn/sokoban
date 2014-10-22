8ea11b77f6979382e0dce1c062a84dde035f863c
Peter Szabados <kangirigungi@gmail.com>
2014-10-22 21:02:55 +0200

    Add more test cases for MovableChecker

--- UnitTest/MovableCheckerTest.cpp
+++ UnitTest/MovableCheckerTest.cpp
@@ -30,4 +30,51 @@ BOOST_AUTO_TEST_CASE(not_blocked_when_there_is_a_single_stone_in_the_middle)
 	BOOST_CHECK(movableCheckerUnderTest.check(data.second, Point{2, 2}));
 }
 
+BOOST_AUTO_TEST_CASE(not_blocked_when_there_are_multiple_stones_in_the_middle_horizontally)
+{
+	auto data = createStatus(5, 5, {
+			"y....",
+			".....",
+			".ooo.",
+			".....",
+			"x...."
+		});
+	MOCK_EXPECT(heurCalculator->calculateStone).returns(1);
+	BOOST_CHECK(movableCheckerUnderTest.check(data.second, Point{1, 2}));
+	BOOST_CHECK(movableCheckerUnderTest.check(data.second, Point{2, 2}));
+	BOOST_CHECK(movableCheckerUnderTest.check(data.second, Point{3, 2}));
+}
+
+BOOST_AUTO_TEST_CASE(not_blocked_when_there_are_multiple_stones_in_the_middle_vertically)
+{
+	auto data = createStatus(5, 5, {
+			"y....",
+			"..o..",
+			"..o..",
+			"..o..",
+			"x...."
+		});
+	MOCK_EXPECT(heurCalculator->calculateStone).returns(1);
+	BOOST_CHECK(movableCheckerUnderTest.check(data.second, Point{2, 1}));
+	BOOST_CHECK(movableCheckerUnderTest.check(data.second, Point{2, 2}));
+	BOOST_CHECK(movableCheckerUnderTest.check(data.second, Point{2, 3}));
+}
+
+BOOST_AUTO_TEST_CASE(not_blocked_when_there_are_multiple_stones_in_the_middle_in_different_shapes)
+{
+	auto data = createStatus(5, 5, {
+			"y....",
+			".oo..",
+			"..o..",
+			"..oo.",
+			"x...."
+		});
+	MOCK_EXPECT(heurCalculator->calculateStone).returns(1);
+	BOOST_CHECK(movableCheckerUnderTest.check(data.second, Point{2, 1}));
+	BOOST_CHECK(movableCheckerUnderTest.check(data.second, Point{2, 2}));
+	BOOST_CHECK(movableCheckerUnderTest.check(data.second, Point{2, 3}));
+	BOOST_CHECK(movableCheckerUnderTest.check(data.second, Point{1, 1}));
+	BOOST_CHECK(movableCheckerUnderTest.check(data.second, Point{3, 3}));
+}
+
 BOOST_AUTO_TEST_SUITE_END()
