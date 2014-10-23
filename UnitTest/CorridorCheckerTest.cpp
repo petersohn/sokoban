ccc4ad4b4048473ec2e5f6012a985f6d98203539
Peter Szabados <kangirigungi@gmail.com>
2014-10-23 10:28:58 +0200

    Add more test cases for CorridorChecker

--- UnitTest/CorridorCheckerTest.cpp
+++ UnitTest/CorridorCheckerTest.cpp
@@ -143,13 +143,26 @@ BOOST_AUTO_TEST_CASE(not_blocked_when_1x1_corridor_can_be_opened_from_one_direct
 	CHECK_CHECKER_RESULT(corridorCheckerUnderTest, data.second, true);
 }
 
-BOOST_AUTO_TEST_CASE(blocked_when_1x1_corridor_cannot_be_opened)
+BOOST_AUTO_TEST_CASE(blocked_when_1x1_corridor_cannot_be_opened1)
 {
 	auto data = createStatus(5, 5, {
 			"y....",
 			"..oo.",
 			".o.o.",
 			".oo..",
+			"x...."
+		});
+	MOCK_EXPECT(heurCalculator->calculateStone).returns(1);
+	CHECK_CHECKER_RESULT(corridorCheckerUnderTest, data.second, false);
+}
+
+BOOST_AUTO_TEST_CASE(blocked_when_1x1_corridor_cannot_be_opened2)
+{
+	auto data = createStatus(5, 5, {
+			"y....",
+			".oo..",
+			".o.o.",
+			"..oo.",
 			"x...."
 		});
 	MOCK_EXPECT(heurCalculator->calculateStone).returns(1);
@@ -219,6 +232,123 @@ BOOST_AUTO_TEST_CASE(blocked_when_horizontal_corridor_cannot_be_opened2)
 		});
 	MOCK_EXPECT(heurCalculator->calculateStone).returns(1);
 	CHECK_CHECKER_RESULT(corridorCheckerUnderTest, data.second, false);
+}
+
+BOOST_AUTO_TEST_CASE(not_blocked_when_vertical_corridor_can_be_opened_from_all_directions)
+{
+	auto data = createStatus(5, 5, {
+			"y.o..",
+			".o.o.",
+			".o.o.",
+			".o.o.",
+			"x.o.."
+		});
+	MOCK_EXPECT(heurCalculator->calculateStone).returns(1);
+	CHECK_CHECKER_RESULT(corridorCheckerUnderTest, data.second, true);
+}
+
+BOOST_AUTO_TEST_CASE(not_blocked_when_vertical_corridor_can_be_opened_from_one_direction)
+{
+	auto data = createStatus(5, 5, {
+			"y.o..",
+			".o.o.",
+			".o.o.",
+			".o.o.",
+			"x.oo."
+		});
+	MOCK_EXPECT(heurCalculator->calculateStone).returns(1);
+	CHECK_CHECKER_RESULT(corridorCheckerUnderTest, data.second, true);
+}
+
+BOOST_AUTO_TEST_CASE(not_blocked_when_vertical_corridor_can_be_opened_from_another_direction)
+{
+	auto data = createStatus(5, 5, {
+			"yoo..",
+			".o.o.",
+			".o.o.",
+			".o.o.",
+			"x.o.."
+		});
+	MOCK_EXPECT(heurCalculator->calculateStone).returns(1);
+	CHECK_CHECKER_RESULT(corridorCheckerUnderTest, data.second, true);
+}
+
+BOOST_AUTO_TEST_CASE(blocked_when_vertical_corridor_cannot_be_opened1)
+{
+	auto data = createStatus(5, 5, {
+			"yoo..",
+			".o.o.",
+			".o.o.",
+			".o.o.",
+			"xoo.."
+		});
+	MOCK_EXPECT(heurCalculator->calculateStone).returns(1);
+	CHECK_CHECKER_RESULT(corridorCheckerUnderTest, data.second, false);
+}
+
+BOOST_AUTO_TEST_CASE(blocked_when_vertical_corridor_cannot_be_opened2)
+{
+	auto data = createStatus(5, 5, {
+			"yoo..",
+			".o.o.",
+			".o.o.",
+			".o.o.",
+			"x.oo."
+		});
+	MOCK_EXPECT(heurCalculator->calculateStone).returns(1);
+	CHECK_CHECKER_RESULT(corridorCheckerUnderTest, data.second, false);
+}
+
+BOOST_AUTO_TEST_CASE(not_blocked_when_corridor_can_be_opened_against_wall1)
+{
+	auto data = createStatus(5, 5, {
+			"y....",
+			"...oo",
+			"..o..",
+			"...oo",
+			"x...."
+		});
+	MOCK_EXPECT(heurCalculator->calculateStone).returns(1);
+	CHECK_CHECKER_RESULT(corridorCheckerUnderTest, data.second, true);
+}
+
+BOOST_AUTO_TEST_CASE(not_blocked_when_corridor_can_be_opened_against_wall2)
+{
+	auto data = createStatus(5, 5, {
+			"y....",
+			".....",
+			"..o..",
+			".o.o.",
+			"xo.o."
+		});
+	MOCK_EXPECT(heurCalculator->calculateStone).returns(1);
+	CHECK_CHECKER_RESULT(corridorCheckerUnderTest, data.second, true);
+}
+
+BOOST_AUTO_TEST_CASE(not_blocked_when_corridor_can_be_opened_against_wall3)
+{
+	auto data = createStatus(5, 5, {
+			"y....",
+			"oo...",
+			"..o..",
+			"oo...",
+			"x...."
+		});
+	MOCK_EXPECT(heurCalculator->calculateStone).returns(1);
+	CHECK_CHECKER_RESULT(corridorCheckerUnderTest, data.second, true);
+}
+
+BOOST_AUTO_TEST_CASE(not_blocked_when_corridor_can_be_opened_against_wall4)
+{
+	auto data = createStatus(5, 5, {
+			"yo.o.",
+			".o.o.",
+			"..o..",
+			".....",
+			"x...."
+		});
+	MOCK_EXPECT(heurCalculator->calculateStone).returns(1);
+	CHECK_CHECKER_RESULT(corridorCheckerUnderTest, data.second, true);
 }
 
 
