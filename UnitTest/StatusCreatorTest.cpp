6b9462c635a6cd015b23cb658e3bbf075c515bd4
eszabpt <peter.szabados@ericsson.com>
2014-11-05 15:52:59 +0100

    Add unit test for StatusCreator

---
+++ UnitTest/StatusCreatorTest.cpp
@@ -0,0 +1,29 @@
+#include "Status/StatusCreator.hpp"
+#include "Status/Status.hpp"
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(StatusCreatorTest)
+
+BOOST_AUTO_TEST_CASE(simple_table)
+{
+	auto data = createStatus(3, 2,{
+			"x.o",
+			"..y"
+		});
+	BOOST_CHECK_EQUAL(data.first->width(), 3);
+	BOOST_CHECK_EQUAL(data.first->height(), 2);
+	BOOST_CHECK_EQUAL(data.first->wall(Point{0, 0}), false);
+	BOOST_CHECK_EQUAL(data.first->wall(Point{1, 0}), false);
+	BOOST_CHECK_EQUAL(data.first->wall(Point{2, 0}), false);
+	BOOST_CHECK_EQUAL(data.first->wall(Point{0, 1}), false);
+	BOOST_CHECK_EQUAL(data.first->wall(Point{1, 1}), false);
+	BOOST_CHECK_EQUAL(data.first->wall(Point{2, 1}), false);
+
+	BOOST_CHECK_EQUAL(&data.second.table(), data.first.get());
+	BOOST_CHECK_EQUAL(data.second.state().size(), 1);
+	BOOST_CHECK_EQUAL(data.second.value(Point{2, 0}), ftStone);
+	BOOST_CHECK_EQUAL(data.second.currentPos(), (Point{2, 1}));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
