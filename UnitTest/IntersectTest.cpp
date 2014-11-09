73692f2f82f4a2dc498d9d7d0692d216d3568fd7
Peter Szabados <kangirigungi@gmail.com>
2014-11-09 13:44:49 +0100

    Add more unit tests for intersect

--- UnitTest/IntersectTest.cpp
+++ UnitTest/IntersectTest.cpp
@@ -15,5 +15,26 @@ BOOST_AUTO_TEST_CASE(distinct_sets)
 	BOOST_CHECK_EQUAL(util::wrap(result), util::wrap(expectedResult));
 }
 
+BOOST_AUTO_TEST_CASE(some_equal_elements)
+{
+	std::vector<int> input1{12, 5, 7, 2, -1, -3};
+	std::vector<int> input2{0, -4, 5, -1, 1};
+	std::vector<int> expectedResult{-1, 5};
+	auto result = intersect(input1, input2);
+	std::sort(result.begin(), result.end());
+	BOOST_CHECK_EQUAL(util::wrap(result), util::wrap(expectedResult));
+}
+
+BOOST_AUTO_TEST_CASE(equal_inputs)
+{
+	std::vector<int> input1{5,2,0,8};
+	std::vector<int> input2{5,2,0,8};
+	std::vector<int> expectedResult{5,2,0,8};
+	auto result = intersect(input1, input2);
+	std::sort(result.begin(), result.end());
+	std::sort(expectedResult.begin(), expectedResult.end());
+	BOOST_CHECK_EQUAL(util::wrap(result), util::wrap(expectedResult));
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
