db5f273de351c95bc244a822b7969092eece1317
Peter Szabados <kangirigungi@gmail.com>
2014-11-11 22:45:16 +0100

    Add more unit tests for IndexedStatusList hasSubStatus

--- UnitTest/IndexedStatusListTest.cpp
+++ UnitTest/IndexedStatusListTest.cpp
@@ -40,6 +40,78 @@ BOOST_AUTO_TEST_CASE(hasSubStatus_contains_one_status)
 	BOOST_CHECK(indexedStatusListUnderTest.hasSubStatus(status, Point{2, 1}));
 }
 
+BOOST_AUTO_TEST_CASE(hasSubStatus_contains_more_statuses)
+{
+	auto status1 = createTestStatus(*table, {
+			".o.",
+			"..o",
+			"y.."});
+	auto status2 = createTestStatus(*table, {
+			".o.",
+			"o..",
+			"y.."});
+	auto status3 = createTestStatus(*table, {
+			"...",
+			".o.",
+			"yoo"});
+	indexedStatusListUnderTest.add(Status{status1}, 0);
+	indexedStatusListUnderTest.add(Status{status2}, 0);
+	indexedStatusListUnderTest.add(Status{status3}, 0);
+
+	BOOST_CHECK(indexedStatusListUnderTest.hasSubStatus(status1, Point{1, 0}));
+	BOOST_CHECK(indexedStatusListUnderTest.hasSubStatus(status1, Point{2, 1}));
+
+	BOOST_CHECK(indexedStatusListUnderTest.hasSubStatus(status2, Point{1, 0}));
+	BOOST_CHECK(indexedStatusListUnderTest.hasSubStatus(status2, Point{0, 1}));
+
+	BOOST_CHECK(indexedStatusListUnderTest.hasSubStatus(status3, Point{1, 1}));
+	BOOST_CHECK(indexedStatusListUnderTest.hasSubStatus(status3, Point{1, 2}));
+	BOOST_CHECK(indexedStatusListUnderTest.hasSubStatus(status3, Point{2, 2}));
+}
+
+BOOST_AUTO_TEST_CASE(hasSubStatus_mismatch)
+{
+	auto status1 = createTestStatus(*table, {
+			".o.",
+			"..o",
+			"y.."});
+	auto status2 = createTestStatus(*table, {
+			".o.",
+			"o..",
+			"y.."});
+	auto status3 = createTestStatus(*table, {
+			"...",
+			".o.",
+			"yoo"});
+	indexedStatusListUnderTest.add(Status{status1}, 0);
+	indexedStatusListUnderTest.add(Status{status2}, 0);
+	indexedStatusListUnderTest.add(Status{status3}, 0);
+
+	auto notExistingStatus = createTestStatus(*table, {
+			".o.",
+			"...",
+			"yo."});
+
+	BOOST_CHECK(!indexedStatusListUnderTest.hasSubStatus(notExistingStatus, Point{1, 0}));
+	BOOST_CHECK(!indexedStatusListUnderTest.hasSubStatus(notExistingStatus, Point{1, 2}));
+}
+
+BOOST_AUTO_TEST_CASE(hasSubStatus_reachability)
+{
+	auto statusOk = createTestStatus(*table, {
+			".o.",
+			"..o",
+			"y.."});
+	auto statusNok = createTestStatus(*table, {
+			".oy",
+			"..o",
+			"..."});
+	indexedStatusListUnderTest.add(Status{statusOk}, 0);
+
+	BOOST_CHECK(!indexedStatusListUnderTest.hasSubStatus(statusNok, Point{1, 0}));
+	BOOST_CHECK(!indexedStatusListUnderTest.hasSubStatus(statusNok, Point{2, 1}));
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
 
