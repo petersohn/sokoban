9359118a96356e532d52458e0bcd403b68273e5b
Peter Szabados <kangirigungi@gmail.com>
2014-11-16 20:07:22 +0100

    Print statistics per second
    Fixes #25

--- src/Dumper/StatisticsDumper.cpp
+++ src/Dumper/StatisticsDumper.cpp
@@ -1,16 +1,57 @@
 #include "Dumper/StatisticsDumper.hpp"
-#include <fstream>
 #include <boost/algorithm/string/join.hpp>
 #include <boost/range/adaptor/map.hpp>
 #include <boost/lexical_cast.hpp>
 
+const boost::posix_time::time_duration StatisticsDumper::printInterval =
+		boost::posix_time::seconds(1);
+
+void StatisticsDumper::update(const std::string& key)
+{
+
+	++globalStatistics[key];
+	++currentStatistics[key];
+
+	auto currentTime = boost::posix_time::microsec_clock::universal_time();
+	if (currentTime > nextPrintTime) {
+		print(nextPrintTime, currentStatistics);
+		nextPrintTime += printInterval;
+
+		for (auto& value: currentStatistics) {
+			value.second = 0;
+		}
+	}
+}
+
+void StatisticsDumper::print(boost::posix_time::ptime time, const Statistics& statistics)
+{
+	if (!headerPrinted) {
+		file << "time;" <<
+			boost::algorithm::join(statistics | boost::adaptors::map_keys, ";") <<
+			std::endl;
+
+		headerPrinted = true;
+	}
+
+	if (time != boost::posix_time::not_a_date_time) {
+		file << time - startTime;
+	}
+
+	file << ";" << boost::algorithm::join(statistics | boost::adaptors::map_values |
+				boost::adaptors::transformed(
+					boost::lexical_cast<std::string, unsigned>), ";") <<
+			std::endl;
+}
+
 void StatisticsDumper::initialStatus(const Status&)
 {
+	startTime = boost::posix_time::microsec_clock::universal_time();
+	nextPrintTime = startTime + printInterval;
 }
 
 void StatisticsDumper::addNode(const Node::Ptr&)
 {
-	++statistics["add node"];
+	update("add node");
 }
 
 void StatisticsDumper::addToSolution(const Node::Ptr&)
@@ -19,32 +60,27 @@ void StatisticsDumper::addToSolution(const Node::Ptr&)
 
 void StatisticsDumper::expand(const Node::Ptr&)
 {
-	++statistics["expand"];
+	update("expand");
 }
 
 void StatisticsDumper::startPushing(const Node::Ptr&)
 {
-	++statistics["push"];
+	update("push");
 }
 
 void StatisticsDumper::push(const Node::Ptr&)
 {
-	++statistics["push node"];
+	update("push node");
 }
 
 void StatisticsDumper::reject(const Node::Ptr&, const char *text)
 {
-	++statistics[std::string{"reject: "} + text];
+	update(std::string{"reject: "} + text);
 }
 
 void StatisticsDumper::save()
 {
-	std::ofstream file{filename, std::ios::out | std::ios::trunc};
-	file << boost::algorithm::join(statistics | boost::adaptors::map_keys, ";") <<
-		"\n" <<
-		boost::algorithm::join(statistics | boost::adaptors::map_values |
-				boost::adaptors::transformed(
-					boost::lexical_cast<std::string, unsigned>), ";") << "\n";
+	print(boost::posix_time::not_a_date_time, globalStatistics);
 }
 
 
--- src/Dumper/StatisticsDumper.hpp
+++ src/Dumper/StatisticsDumper.hpp
@@ -2,18 +2,27 @@
 #define SRC_DUMPER_STATISTICSDUMPER_HPP
 
 #include "Dumper/Dumper.hpp"
+#include "Mutexes.hpp"
 #include <string>
+#include <fstream>
+#include <boost/date_time/posix_time/posix_time.hpp>
 
 class StatisticsDumper: public Dumper {
 	using Statistics = std::map<std::string, unsigned>;
 
-	Statistics statistics;
-	std::string filename;
+	Statistics currentStatistics, globalStatistics;
+	std::ofstream file;
+	bool headerPrinted = false;
+	boost::posix_time::ptime startTime, nextPrintTime;
 
+	void update(const std::string& key);
+	void print(boost::posix_time::ptime time, const Statistics& statistics);
+
+	static const boost::posix_time::time_duration printInterval;
 public:
 
-	StatisticsDumper(std::string filename): 
-		filename(std::move(filename))
+	StatisticsDumper(const std::string& filename):
+		file(filename, std::ios::out | std::ios::trunc)
 	{}
 
 	void initialStatus(const Status &status) override;
