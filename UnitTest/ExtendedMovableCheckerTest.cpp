225717b80c7f35264bec225e38aa010926613781
Peter Szabados <kangirigungi@gmail.com>
2014-11-29 18:26:20 +0100

    Add more unit test for indirectly movable stone

--- UnitTest/ExtendedMovableCheckerTest.cpp
+++ UnitTest/ExtendedMovableCheckerTest.cpp
@@ -354,7 +354,7 @@ BOOST_AUTO_TEST_CASE(not_movable_when_source_point_not_reachable_horizontally)
 	CHECK_CHECKER_RESULT(movableCheckerUnderTest, data.second, false);
 }
 
-BOOST_AUTO_TEST_CASE(movable_when_stone_is_movable_indirectly)
+BOOST_AUTO_TEST_CASE(movable_when_stone_is_movable_indirectly_vertically)
 {
 	auto data = createStatus(5, 5, {
 			"y.*..",
@@ -364,6 +364,20 @@ BOOST_AUTO_TEST_CASE(movable_when_stone_is_movable_indirectly)
 			"x.*.."
 		});
 	MOCK_EXPECT(heurCalculator->calculateStone).with(mock::any, Point{4, 3}).returns(-1);
+	MOCK_EXPECT(heurCalculator->calculateStone).returns(1);
+	CHECK_CHECKER_RESULT(movableCheckerUnderTest, data.second, true);
+}
+
+BOOST_AUTO_TEST_CASE(movable_when_stone_is_movable_indirectly_horizontally)
+{
+	auto data = createStatus(5, 5, {
+			"y....",
+			".....",
+			"*o*o*",
+			"...o.",
+			"x...."
+		});
+	MOCK_EXPECT(heurCalculator->calculateStone).with(mock::any, Point{3, 4}).returns(-1);
 	MOCK_EXPECT(heurCalculator->calculateStone).returns(1);
 	CHECK_CHECKER_RESULT(movableCheckerUnderTest, data.second, true);
 }
