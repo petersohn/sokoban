741bea5be22f48a8ba003631f6445564184f4561
eszabpt <peter.szabados@ericsson.com>
2014-12-04 17:28:02 +0100

    Add test case testing multiple stones for SolutionChecker

--- UnitTest/SolutionCheckerTest.cpp
+++ UnitTest/SolutionCheckerTest.cpp
@@ -13,10 +13,11 @@ struct SolutionCheckerTestFixture {
 
 	SolutionCheckerTestFixture()
 	{
-		auto data = createStatus(5, 5, {
+		auto data = createStatus(5, 6, {
 				"xo...",
 				"....*",
 				"**.**",
+				".....",
 				".....",
 				"y.*.."});
 		table = std::move(data.first);
@@ -64,9 +65,69 @@ BOOST_AUTO_TEST_CASE(good_solution)
 				"....*",
 				"**o**",
 				".....",
+				".....",
 				"y.*.."});
 
 	std::deque<std::shared_ptr<Node>> nodes{createNodes({
+		NodeData{
+			State{Point{2, 1}},
+			MoveDescriptor{Point{2, 2}, Point{0, -1}},
+		},
+		NodeData{
+			State{Point{2, 0}},
+			MoveDescriptor{Point{2, 1}, Point{0, -1}},
+		},
+		NodeData{
+			State{Point{1, 0}},
+			MoveDescriptor{Point{2, 0}, Point{-1, 0}},
+		},
+		NodeData{
+			State{},
+			MoveDescriptor{Point{1, 0}, Point{-1, 0}},
+		}
+		})};
+	BOOST_CHECK(solutionCheckerUnderTest.checkResult(initialStatus, nodes));
+}
+
+BOOST_AUTO_TEST_CASE(good_solution_with_multiple_stones)
+{
+	auto initialStatus = createTestStatus(*table, {
+				"x....",
+				"....*",
+				"**o**",
+				"..o..",
+				".....",
+				"y.*.."});
+
+	std::deque<std::shared_ptr<Node>> nodes{createNodes({
+		NodeData{
+			State{Point{2, 2}, Point{3, 3}},
+			MoveDescriptor{Point{2, 3}, Point{1, 0}},
+		},
+		NodeData{
+			State{Point{2, 1}, Point{3, 3}},
+			MoveDescriptor{Point{2, 2}, Point{0, -1}},
+		},
+		NodeData{
+			State{Point{2, 0}, Point{3, 3}},
+			MoveDescriptor{Point{2, 1}, Point{0, -1}},
+		},
+		NodeData{
+			State{Point{1, 0}, Point{3, 3}},
+			MoveDescriptor{Point{2, 0}, Point{-1, 0}},
+		},
+		NodeData{
+			State{Point{3, 3}},
+			MoveDescriptor{Point{1, 0}, Point{-1, 0}},
+		},
+		NodeData{
+			State{Point{2, 3}},
+			MoveDescriptor{Point{3, 3}, Point{-1, 0}},
+		},
+		NodeData{
+			State{Point{2, 2}},
+			MoveDescriptor{Point{2, 3}, Point{0, -1}},
+		},
 		NodeData{
 			State{Point{2, 1}},
 			MoveDescriptor{Point{2, 2}, Point{0, -1}},
