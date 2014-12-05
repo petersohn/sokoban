c98c74ba100e03452cd2e23c2a83226dcf6d32b2
Peter Szabados <kangirigungi@gmail.com>
2014-12-05 21:38:25 +0100

    Refactor ExtendedMovableCheckerTest

--- UnitTest/ExtendedMovableCheckerTest.cpp
+++ UnitTest/ExtendedMovableCheckerTest.cpp
@@ -2,17 +2,13 @@
 #include "Status/StatusCreator.hpp"
 #include "MockHeurCalculator.hpp"
 #include "CheckerTest.hpp"
+#include "MovableCheckerFixture.hpp"
 #include <boost/test/unit_test.hpp>
 
-struct ExtendedMovableCheckerFixture {
-	std::shared_ptr<MockHeurCalculator> heurCalculator =
-		std::make_shared<MockHeurCalculator>();
-	ExtendedMovableChecker movableCheckerUnderTest{heurCalculator};
-};
+BOOST_FIXTURE_TEST_SUITE(ExtendedMovableCheckerTest,
+		MovableCheckerFixture<ExtendedMovableChecker>)
 
-BOOST_FIXTURE_TEST_SUITE(ExtendedMovableCheckerTest, ExtendedMovableCheckerFixture)
-
-BOOST_AUTO_TEST_CASE(not_movable_when_source_point_not_reachable_vertically)
+BOOST_AUTO_TEST_CASE(blocked_when_source_point_not_reachable_vertically)
 {
 	auto data = createStatus(5, 5, {
 			"y....",
@@ -26,7 +22,7 @@ BOOST_AUTO_TEST_CASE(not_movable_when_source_point_not_reachable_vertically)
 	CHECK_CHECKER_RESULT(movableCheckerUnderTest, data.second, false);
 }
 
-BOOST_AUTO_TEST_CASE(not_movable_when_source_point_not_reachable_horizontally)
+BOOST_AUTO_TEST_CASE(blocked_when_source_point_not_reachable_horizontally)
 {
 	auto data = createStatus(5, 5, {
 			"y....",
