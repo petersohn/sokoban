507757d54f4130643294d9299251e3bfda8b063c
eszabpt <peter.szabados@ericsson.com>
2014-12-05 16:55:12 +0100

    Move positive Extended/MovableChecker test cases to separate file

---
+++ UnitTest/MovableCheckerFixture.hpp
@@ -0,0 +1,16 @@
+#ifndef SRC_MOVABLECHECKERFIXTURE_HPP
+#define SRC_MOVABLECHECKERFIXTURE_HPP
+
+#include "MockHeurCalculator.hpp"
+#include <memory>
+
+template <typename Checker>
+struct MovableCheckerFixture {
+	std::shared_ptr<MockHeurCalculator> heurCalculator =
+		std::make_shared<MockHeurCalculator>();
+	Checker movableCheckerUnderTest{heurCalculator};
+};
+
+
+
+#endif /* SRC_MOVABLECHECKERFIXTURE_HPP */
