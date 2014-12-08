5e173ac8462bdf317f5424da3d3ec4d7be517e7a
eszabpt <peter.szabados@ericsson.com>
2014-12-08 16:49:51 +0100

    Check that solution moves at every step

--- UnitTest/SolutionCheckerTest.cpp
+++ UnitTest/SolutionCheckerTest.cpp
@@ -236,7 +236,42 @@ BOOST_AUTO_TEST_CASE(stone_is_moved_in_too_much)
 	BOOST_CHECK(!solutionCheckerUnderTest.checkResult(initialStatus, nodes));
 }
 
-BOOST_AUTO_TEST_SUITE_END() // good_solutions
+BOOST_AUTO_TEST_CASE(stone_is_not_moved)
+{
+	auto initialStatus = createTestStatus(*table, {
+				"x....",
+				"....*",
+				"**o**",
+				".....",
+				".....",
+				"y.*.."});
+
+	std::deque<std::shared_ptr<Node>> nodes{createNodes({
+		NodeData{
+			State{Point{2, 1}},
+			MoveDescriptor{Point{2, 2}, Point{0, -1}},
+		},
+		NodeData{
+			State{Point{2, 1}},
+			MoveDescriptor{Point{2, 1}, Point{0, 0}},
+		},
+		NodeData{
+			State{Point{2, 0}},
+			MoveDescriptor{Point{2, 1}, Point{0, -1}},
+		},
+		NodeData{
+			State{Point{1, 0}},
+			MoveDescriptor{Point{2, 0}, Point{-1, 0}},
+		},
+		NodeData{
+			State{},
+			MoveDescriptor{Point{1, 0}, Point{-1, 0}},
+		}
+		})};
+	BOOST_CHECK(!solutionCheckerUnderTest.checkResult(initialStatus, nodes));
+}
+
+BOOST_AUTO_TEST_SUITE_END() // invalid_moves
 
 BOOST_AUTO_TEST_SUITE(inconsistent_nodes)
 
--- src/SolutionChecker.cpp
+++ src/SolutionChecker.cpp
@@ -52,9 +52,9 @@ bool SolutionChecker::checkResult(const Status& initialStatus, const std::deque<
 	int resultLength = solution.back()->cost();
 	for(std::shared_ptr<Node> node: solution) {
 		Point to(node->from() + node->d());
-		if (std::abs(node->d().x) + std::abs(node->d().y) > 1) {
+		if (std::abs(node->d().x) + std::abs(node->d().y) != 1) {
 			result = false;
-			printError(oldNode, node, status, "Move is too big.");
+			printError(oldNode, node, status, "Invalid move.");
 		}
 		if (to != status.table().destination() && !node->state()[to]) {
 			result = false;
