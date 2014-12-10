82af8ec0aa7e3c2512d87161acac54e450fa0dbb
eszabpt <peter.szabados@ericsson.com>
2014-12-10 10:31:30 +0100

    Make CorridorChecker header only

--- src/CorridorChecker.hpp
+++ src/CorridorChecker.hpp
@@ -4,7 +4,9 @@
 #include "CorridorCheckerBase.hpp"
 #include "HeurCalculator.hpp"
 #include "Array.hpp"
+#include "Status/Status.hpp"
 #include "Status/floodFill.hpp"
+#include "FieldType.hpp"
 
 class CorridorCheckerStrategy {
 	const HeurCalculator* calculator_;
@@ -14,8 +16,20 @@ public:
 			const Status& status):
 		calculator_(calculator), status(status)
 	{}
-	bool checkCorridorEnding(Point p0, Point side) const;
-	void floodFill(Point p0, Array<bool>& result, MinMax& minmax);
+	bool checkCorridorEnding(Point p0, Point side) const
+	{
+		Point p1 = p0 + side;
+		Point pm1 = p0 - side;
+		return status.value(p0) != FieldType::wall &&
+				status.value(p1) == FieldType::floor &&
+				status.value(pm1) == FieldType::floor &&
+				(calculator_->calculateStone(status, p1) >= 0 ||
+				 calculator_->calculateStone(status, pm1) >= 0);
+	}
+	void floodFill(Point p0, Array<bool>& result, MinMax& minmax)
+	{
+		::floodFill(status, p0, result, minmax);
+	}
 };
 
 class CorridorCheckerStrategyFactory {
--- src/ExtendedCorridorChecker.hpp
+++ src/ExtendedCorridorChecker.hpp
@@ -5,6 +5,9 @@
 #include "HeurCalculator.hpp"
 #include "Array.hpp"
 #include "Status/floodFill.hpp"
+#include "Status/Status.hpp"
+#include "Status/floodFill.hpp"
+#include "FieldType.hpp"
 #include <memory>
 
 class ExtendedCorridorCheckerStrategy {
@@ -21,8 +24,23 @@ public:
 			const Status& status):
 		calculator_(calculator), status(status)
 	{}
-	bool checkCorridorEnding(Point p0, Point side) const;
-	void floodFill(Point p0, Array<bool>& result, MinMax& minmax);
+	bool checkCorridorEnding(Point p0, Point side) const
+	{
+		Point p1 = p0 + side;
+		Point pm1 = p0 - side;
+		return status.value(p0) != FieldType::wall &&
+				status.value(p1) == FieldType::floor &&
+				status.value(pm1) == FieldType::floor &&
+				(isMovableTo(p1, pm1) || isMovableTo(pm1, p1));
+	}
+
+	void floodFill(Point p0, Array<bool>& result, MinMax& minmax)
+	{
+		Status::BorderType border;
+		::floodFill(status, p0, result, minmax, border);
+		simpleStatus.reset(new Status{status.table(),
+			State{std::move(std::move(border))}, status.currentPos()});
+	}
 };
 
 class ExtendedCorridorCheckerStrategyFactory {
