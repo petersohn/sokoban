9c68c80aa61a31856c2274ae27946a22b80c326f
Peter Szabados <kangirigungi@gmail.com>
2014-12-24 10:22:29 +0100

    Add more unit test for IndexedStatusList

--- UnitTest/IndexedStatusListTest.cpp
+++ UnitTest/IndexedStatusListTest.cpp
@@ -75,6 +75,21 @@ BOOST_AUTO_TEST_CASE(hasSubStatus_finds_proper_sub_status)
 	BOOST_CHECK(indexedStatusListUnderTest.hasSubStatus(superStatus, Point{2, 1}));
 }
 
+BOOST_AUTO_TEST_CASE(hasSubStatus_finds_proper_sub_status_only_for_points_within_sub_status)
+{
+	auto status = createTestStatus(*table, {
+			".o.",
+			"..o",
+			"y.."});
+	indexedStatusListUnderTest.add(Status{status});
+	auto superStatus = createTestStatus(*table, {
+			".o.",
+			"o.o",
+			"y.."});
+	BOOST_CHECK_EQUAL(indexedStatusListUnderTest.size(), 1);
+	BOOST_CHECK(!indexedStatusListUnderTest.hasSubStatus(superStatus, Point{0, 1}));
+}
+
 BOOST_AUTO_TEST_CASE(hasSubStatus_contains_more_statuses)
 {
 	auto status1 = createTestStatus(*table, {
@@ -103,6 +118,39 @@ BOOST_AUTO_TEST_CASE(hasSubStatus_contains_more_statuses)
 	BOOST_CHECK(indexedStatusListUnderTest.hasSubStatus(status3, Point{1, 1}));
 	BOOST_CHECK(indexedStatusListUnderTest.hasSubStatus(status3, Point{1, 2}));
 	BOOST_CHECK(indexedStatusListUnderTest.hasSubStatus(status3, Point{2, 2}));
+}
+
+BOOST_AUTO_TEST_CASE(hasSubStatus_works_for_multiple_proper_sub_statuses)
+{
+	auto status1 = createTestStatus(*table, {
+			".o.",
+			"..o",
+			"y.."});
+	auto status2 = createTestStatus(*table, {
+			".o.",
+			"o..",
+			"y.."});
+	auto status3 = createTestStatus(*table, {
+			"...",
+			".o.",
+			"yoo"});
+	indexedStatusListUnderTest.add(Status{status1});
+	indexedStatusListUnderTest.add(Status{status2});
+	indexedStatusListUnderTest.add(Status{status3});
+
+	auto superStatus = createTestStatus(*table, {
+			".o.",
+			"o.o",
+			"y.."});
+
+	BOOST_CHECK_EQUAL(indexedStatusListUnderTest.size(), 3);
+	BOOST_CHECK(indexedStatusListUnderTest.hasSubStatus(superStatus, Point{1, 0}));
+	BOOST_CHECK(indexedStatusListUnderTest.hasSubStatus(superStatus, Point{2, 1}));
+	BOOST_CHECK(indexedStatusListUnderTest.hasSubStatus(superStatus, Point{0, 1}));
+
+	BOOST_CHECK(!indexedStatusListUnderTest.hasSubStatus(superStatus, Point{1, 1}));
+	BOOST_CHECK(!indexedStatusListUnderTest.hasSubStatus(superStatus, Point{1, 2}));
+	BOOST_CHECK(!indexedStatusListUnderTest.hasSubStatus(superStatus, Point{2, 2}));
 }
 
 BOOST_AUTO_TEST_CASE(hasSubStatus_mismatch)
